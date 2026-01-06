c0e7d48fe0d1dd9c73921eddd9a2557b0231d54f
Hermann <188955756+4483-s@users.noreply.github.com>
2026-01-07 00:02:09 +0800

    nvim

---
+++ nvim/dot-config/nvim/init.lua
@@ -0,0 +1,12 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.keymap.set("n", "<leader>o", function()
+  vim.api.nvim_put({ "()=>{}" }, "c", true, true)
+end)
+local s = vim.keymap.set
+local p = vim.api.nvim_put
+s("n", "<leader>jj", function()
+  p({ "()=>{}" }, "c", false, true)
+  vim.api.nvim_input("hi")
+end)
+-- vim.keymap.set("i", "x", "w")

9f2fb229df81014dac8d51dfd4ca0d8ef2705d16
Hermann <188955756+4483-s@users.noreply.github.com>
2026-03-28 16:05:08 +0800

    sh

--- home/dot-shaddon.d/env.sh
+++ home/dot-shaddon.d/env.sh
@@ -1,6 +1,7 @@
 export TERM=xterm-256color
 export BROWSER='waterfox'
 export EDITOR='nvim'
+export PATH=~/.local/bin:${PATH}
 # export MANPAGER='nvim +Man!'
 # export AGNOSTER_DIR_BG=56
 # export AGNOSTER_DIR_FG=193

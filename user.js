e66007e1bb6da1d3096ba5c28e32d9ffb09b6bb2
Hermann <188955756+4483-s@users.noreply.github.com>
2026-07-05 20:21:16 +0800

    user.js

--- user.js
+++ user.js
@@ -1,2 +1,4 @@
 user_pref("browser.uidensity", 1);
-user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0");
+user_pref("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0");
+user_pref("signon.rememberSignons", false);
+user_pref("geo.provider.use_geoclue", false);

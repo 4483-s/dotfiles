2a729aa9f867eedef51a94a3fbbe1f636f85aaf9
Hermann <188955756+4483-s@users.noreply.github.com>
2026-07-07 13:40:06 +0800

    fox

---
+++ fox/common.js
@@ -0,0 +1,20 @@
+user_pref("browser.uidensity", 1); // smaller top bar
+user_pref("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0");
+user_pref("signon.rememberSignons", false);
+user_pref("geo.provider.use_geoclue", false);
+user_pref("layout.css.prefers-color-scheme.content-override", 0); // dark color scheme
+user_pref("browser.aboutConfig.showWarning", false);
+
+
+
+user_pref("browser.startup.page", 0);
+user_pref("browser.startup.homepage", "chrome://browser/content/blanktab.html");
+user_pref("browser.newtabpage.enabled", false);
+
+user_pref("privacy.globalprivacycontrol.enabled", true);
+user_pref("browser.uitour.enabled", false);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.translations.automaticallyPopup", false);
+user_pref("browser.search.separatePrivateDefault", false);
+
+
---
+++ fox/waterfox.js
@@ -0,0 +1,3 @@
+user_pref("waterfox.blocker.enabled", false);
+user_pref("waterfox.blocker.extensionDetectionDismissed", true);
+user_pref("waterfox.blocker.ui.enabled", false);

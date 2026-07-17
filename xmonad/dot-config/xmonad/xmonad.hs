4a4d61447d5c04634e68d1a50d0ae18106f7f029
Hermann <188955756+4483-s@users.noreply.github.com>
2026-07-17 22:36:07 +0800

    update

--- xmonad/dot-config/xmonad/xmonad.hs
+++ xmonad/dot-config/xmonad/xmonad.hs
@@ -1,75 +1,13 @@
--- -- Base
 import XMonad
--- import System.Directory
--- import System.IO (hClose, hPutStr, hPutStrLn)
--- import System.Exit (exitSuccess)
 import qualified XMonad.StackSet as W
---
---     -- Actions
--- import XMonad.Actions.CopyWindow (kill1)
--- import XMonad.Actions.CycleWS (Direction1D(..), moveTo, shiftTo, WSType(..), nextScreen, prevScreen)
--- import XMonad.Actions.GridSelect
--- import XMonad.Actions.MouseResize
--- import XMonad.Actions.Promote
--- import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
--- import XMonad.Actions.WindowGo (runOrRaise)
--- import XMonad.Actions.WithAll (sinkAll, killAll)
--- import qualified XMonad.Actions.Search as S
---
---     -- Data
--- import Data.Char (isSpace, toUpper)
--- import Data.Maybe (fromJust)
 import Data.Monoid
--- import Data.Maybe (isJust)
--- import Data.Tree
 import qualified Data.Map as M
---
---     -- Hooks
--- import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
 import XMonad.Hooks.EwmhDesktops  -- for some fullscreen events, also for xcomposite in obs.
--- import XMonad.Hooks.ManageDocks (avoidStruts, docks, manageDocks, ToggleStruts(..))
--- import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat, doCenterFloat)
--- import XMonad.Hooks.ServerMode
 import XMonad.Hooks.SetWMName
--- import XMonad.Hooks.StatusBar
--- import XMonad.Hooks.StatusBar.PP
--- import XMonad.Hooks.WindowSwallowing
--- import XMonad.Hooks.WorkspaceHistory
---
---     -- Layouts
--- import XMonad.Layout.Accordion
--- import XMonad.Layout.GridVariants (Grid(Grid))
--- import XMonad.Layout.SimplestFloat
--- import XMonad.Layout.Spiral
--- import XMonad.Layout.ResizableTile
--- import XMonad.Layout.Tabbed
--- import XMonad.Layout.ThreeColumns
---
---     -- Layouts modifiers
--- import XMonad.Layout.LayoutModifier
--- import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
--- import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), (??))
--- import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
 import XMonad.Layout.NoBorders
--- import XMonad.Layout.Renamed
--- import XMonad.Layout.ShowWName
--- import XMonad.Layout.Simplest
--- import XMonad.Layout.Spacing
--- import XMonad.Layout.SubLayouts
--- import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
--- import XMonad.Layout.WindowNavigation
--- import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
--- import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
---
---    -- Utilities
--- import XMonad.Util.Dmenu
 import XMonad.Util.EZConfig (additionalKeysP, mkNamedKeymap)
--- import XMonad.Util.Hacks (windowedFullscreenFixEventHook, javaHack, trayerAboveXmobarEventHook, trayAbovePanelEventHook, trayerPaddingXmobarEventHook, trayPaddingXmobarEventHook, trayPaddingEventHook)
--- import XMonad.Util.NamedActions
--- import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
 import XMonad.Util.SpawnOnce
---------
 import XMonad.Actions.Submap
 import System.Exit
 import XMonad.Hooks.ManageDocks
@@ -92,7 +30,6 @@ spawnBasedOnFocus = withFocused $ \w -> do
           | "Alacritty" `isInfixOf` c -> spawn "alacritty"
         _                             -> spawn "firefox"
 
--- myTerminal      = "ghostty"
 myTerminal      = "kitty"
 
 myFocusFollowsMouse :: Bool
@@ -122,35 +59,32 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
 ezKeybindings = [ 
--- ("M-S-z", spawn "xscreensaver-command -lock")
 -- , ("M-C-s", unGrab *> spawn "scrot -s"        )
-           -- ("M-;"        , spawn myTerminal)
-           ("M-["        , spawn myTerminal)
+           ("M-a"        , spawn       "wmscript-a"  ) -- test
+        ,  ("M-["        , spawn myTerminal)
         ,  ("M-;"        , spawn myTerminal)
         ,  ("M-/"        , spawn "dmtype.sh")
-        ,  ("M-m"        , unGrab >> spawn "xdotool key --clearmodifiers ctrl+Tab")
-        ,  ("M-n"        , unGrab >> spawn "xdotool key --clearmodifiers ctrl+shift+Tab")
---      ,  ("M-i"        , unGrab >> spawn "xdotool key --clearmodifiers ctrl+w")
+        -- ,  ("M-m"        , unGrab >> spawn "xdotool key --clearmodifiers ctrl+Tab")
+        -- ,  ("M-n"        , unGrab >> spawn "xdotool key --clearmodifiers ctrl+shift+Tab")
+        ,  ("M-m"        , unGrab >> spawn "xdo2.sh")
+        ,  ("M-n"        , unGrab >> spawn "xdo1.sh")
         ,  ("M-o"        , spawn "waterfox")
         ,  ("M-p"        , spawn "rofi -show drun -m -1")
         ,  ("M-y"        , kill)
-        ,  ("M-l"        , windows W.focusDown)
-        ,  ("M-h"        , windows W.focusUp)
         ,  ("M-S-q"      , io exitSuccess)
         ,  ("M-q"        , spawn "xmonad --recompile && xmonad --restart && echo 8 > ~/.xmonadcmp.log")
-        -- ,  ("M-j"        , windows W.focusDown)
-        -- ,  ("M-k"        , windows W.focusUp)
         ,  ("M-."        , nextScreen)
         ,  ("M-,"        , prevScreen)
         ,  ("M-<Space>"  , sendMessage NextLayout)
         ,  ("M-<Return>" , zshPrompt def "/home/h/.local/bin/capture.zsh")
-        ,  ("M--"        , sendMessage Shrink)
+        ,  ("M-h"        , sendMessage Shrink)
+        ,  ("M-l"        , sendMessage Expand)
         ,  ("M-b"        , spawnBasedOnFocus)
-        ,  ("M-="        , sendMessage Expand)
         ,  ("M-S-j"      , windows W.swapDown)
         ,  ("M-S-k"      , windows W.swapUp)
         ,  ("M-t"        , withFocused $ windows . W.sink)
 --        ,  ("M-,"        , sendMessage (IncMasterN 1))
+        ,  ("M-e"        , windows W.swapMaster)
         ,  ("M-<F9>"     , spawn "bash -c '[[ $(setxkbmap -query | grep -P layout:.*ru ) ]] && setxkbmap us || setxkbmap ru'")
         ,  ("M-<F2>"     , spawn "pactl set-sink-volume @DEFAULT_SINK@ -1%")
         ,  ("M-<F3>"     , spawn "pactl set-sink-volume @DEFAULT_SINK@ +1%")
@@ -176,21 +110,34 @@ ezKeybindings = [
      ] ++ [
     ("M-u", submap . M.fromList $
        [
-        ((0, xK_y),         spawn "wmscript-y.sh"),
-        ((0, xK_u),         spawn "wmscript-u.sh"),
-        ((0, xK_i),         spawn "wmscript-i.sh"),
-        ((0, xK_o),         spawn "wmscript-o.sh"),
-        ((0, xK_p),         spawn "wmscript-p.sh"),
-        ((0, xK_h),         spawn "wmscript-h.sh"),
-        ((0, xK_j),         spawn "wmscript-j.sh"),
-        ((0, xK_k),         spawn "wmscript-k.sh"),
-        ((0, xK_l),         spawn "wmscript-l.sh"),
-        ((0, xK_semicolon), spawn "wmscript-semicolon.sh"),
-        ((0, xK_n),         spawn "wmscript-n.sh"),
-        ((0, xK_m),         spawn "wmscript-m.sh"),
-        ((0, xK_comma),     spawn "wmscript-comma.sh"),
-        ((0, xK_period),    spawn "wmscript-peroid.sh"),
-        ((0, xK_slash),     spawn "kitty")
+        ((0, xK_1),         spawn "wmscript-u-1"),
+        ((0, xK_2),         spawn "wmscript-u-2"),
+        ((0, xK_3),         spawn "wmscript-u-3"),
+        ((0, xK_4),         spawn "wmscript-u-4"),
+        ((0, xK_5),         spawn "wmscript-u-5"),
+        ((0, xK_6),         spawn "wmscript-u-6"),
+        ((0, xK_7),         spawn "wmscript-u-7"),
+        ((0, xK_8),         spawn "wmscript-u-8"),
+        ((0, xK_9),         spawn "wmscript-u-9"),
+        ((0, xK_0),         spawn "wmscript-u-0"),
+
+        ((0, xK_y),         spawn "wmscript-u-y"),
+        ((0, xK_u),         spawn "wmscript-u-u"),
+        ((0, xK_i),         spawn "wmscript-u-i"),
+        ((0, xK_o),         spawn "wmscript-u-o"),
+        ((0, xK_p),         spawn "wmscript-u-p"),
+        ((0, xK_h),         spawn "wmscript-u-h"),
+        ((0, xK_j),         spawn "wmscript-u-j"),
+        ((0, xK_k),         spawn "wmscript-u-k"),
+        ((0, xK_l),         spawn "wmscript-u-l"),
+        ((0, xK_n),         spawn "wmscript-u-n"),
+        ((0, xK_m),         spawn "wmscript-u-m"),
+        ((0, xK_q),         spawn "wmscript-u-q"), -- test
+        ((0, xK_semicolon), spawn "wmscript-u-semicolon"),
+        ((0, xK_comma),     spawn "wmscript-u-comma"),
+        ((0, xK_period),    spawn "wmscript-u-peroid"),
+        ((0, xK_slash),     spawn "wmscript-u-slash"),
+        ((0, xK_Return),    spawn "wmscript-u-Return")
        ])
      ]
      -- ++ 
@@ -198,11 +145,6 @@ ezKeybindings = [
      --        | c <- ["alacritty"] 
      --        , let keyBinding = ("M-d", spawn "kitty")
      --        ]
-     -- my2D = navigation2DP def
---                               ("<Up>", "<Left>", "<Down>", "<Right>")
---                               [("M-",   windowGo  ),
---                                ("M-S-", windowSwap)]
---                               False
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList
     -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modm, button1), \w -> focus w >> mouseMoveWindow w
@@ -287,9 +229,8 @@ myStartupHook = do
   setWMName "LG3D"
 
 main = do 
-    -- xmproc <- spawnPipe "xmobar -x 0 /home/h/.config/xmobar/myxmo"
-    -- xmproc <- spawnPipe "polybar mainbar-xmonad"
     xmonad $ docks $ ewmh $ ewmhFullscreen $ defaults
+    -- xmproc <- spawnPipe "polybar mainbar-xmonad"
     -- xmonad $ docks $ ewmh $ ewmhFullscreen $ my2D $ defaults
 
 defaults = def {

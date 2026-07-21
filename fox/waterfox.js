6109b72c9e3ba056ebddc757145e1cae1a95658e
Hermann <188955756+4483-s@users.noreply.github.com>
2026-07-21 22:47:04 +0800

    update

--- fox/waterfox.js
+++ fox/waterfox.js
@@ -1,3 +1,8 @@
 user_pref("waterfox.blocker.enabled", false);
 user_pref("waterfox.blocker.extensionDetectionDismissed", true);
 user_pref("waterfox.blocker.ui.enabled", false);
+user_pref("waterfox.blocker.allowSearchPartnerAds", false);
+user_pref("waterfox.blocker.showBadge", false);
+user_pref("waterfox.blocker.remoteResourcesEnabled", false);
+
+
